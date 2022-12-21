edeb27000612d94c3daece390c3cd706a6897dbf
zachorywhite <zach88white@gmail.com>
2022-12-21 11:57:11 -0800

    UI and API initialization
    Created angular app, basic table no API connection. Created API structure, tested with mock data

---
+++ dotang-prac-API/dotang-prac-API/Controllers/SuperHeroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotang_prac_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero> { 
+                new SuperHero {
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                } 
+            };
+        }
+    }
+}
